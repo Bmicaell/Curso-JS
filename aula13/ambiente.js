01fc852f8bfa0e6d8acadf710bbbdcf7535f9fe5
Bmicaell <99100170+Bmicaell@users.noreply.github.com>
2025-04-11 17:40:14 -0300

    WHILE DO e DO WHILE

---
+++ aula13/ambiente.js
@@ -0,0 +1,12 @@
+var c = 1;
+
+while (c <= 6) { //estrutura de repetição com teste logico no inicio
+
+console.log("Hello World");
+    c++
+}
+
+do {
+    console.log(`Passo ${c}`);
+    c++
+} while  (c <= 6) //estrutura de repetição com teste logico no final
