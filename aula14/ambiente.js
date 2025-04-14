dab325f5d434e3b6c41b1ad32a3ff62a05ab6778
Bmicaell <99100170+Bmicaell@users.noreply.github.com>
2025-04-14 16:36:26 -0300

    Estrutura de repitição - FOR

---
+++ aula14/ambiente.js
@@ -0,0 +1,14 @@
+/*
+var c = 1
+while ( c <= 5) { //estrutura de repetição com teste logico no inicio
+    console.log(c)
+    c++ //incrementa o valor de c em 1
+}
+*/
+//--------------------
+console.log("VAI COMEÇAR...")
+for(var c = 1; c <= 5; c++) { //estrutura de repetição com teste logico no inicio
+    console.log(c)
+}
+console.log("FIM!")
+//--------------------
