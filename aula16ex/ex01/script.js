5a5e186114063d1f236552f0960f21cba79be61c
Bmicaell <99100170+Bmicaell@users.noreply.github.com>
2025-04-19 17:08:54 -0300

    EX 01 - Funções

--- aula16ex/ex01/script.js
+++ aula16ex/ex01/script.js
@@ -5,14 +5,39 @@ function addNumber() {
     var textarea = document.getElementById('ival')
     var n = Number(num)
 
-    if(num.length == 0 || num < 0 || num > 100 ) {
+    if(num.length == 0 || num < 0 || num > 100 || valores.includes(n) ) {
         alert('[ERRO] Valor inválido ou número ja possui na lista')
         
+    }else  { 
+        valores.push(n) 
+        textarea.value += ` Valor ${n} adicionado.\n`
     }
-        if(valores.includes(n)){
-            alert('[ERRO] Número ja possui na lista')
-        } else  { 
-            valores.push(n) 
-            textarea.value = valores.join('\n')
+}
+
+function analisarNum() {
+    var res = document.getElementById('res')
+    var maiorNum = valores[0]
+    var menorNum = valores[0]
+    var soma = valores
+
+    res.innerHTML = ''
+    res.innerHTML += `Ao todo, temos ${valores.length} números cadastro. <br>` 
+
+    for(var i = 0; i < valores.length; i++) {
+        if(valores[i] > maiorNum) {
+            maiorNum = valores[i]
+        }  
+        if (valores[i] < menorNum ) {
+            menorNum = valores[i]
         }
+
+        soma += valores
+    }
+    
+    res.innerHTML += `O maior valor informado foi ${maiorNum}. <br>`
+    res.innerHTML += `O menor valor informado foi ${menorNum}. <br>`
+    res.innerHTML += `A soma dos valores ${soma}. <br>`
+
+    
+    
 }
